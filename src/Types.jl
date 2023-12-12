c3dc429a39dcf425833fb7e2d51af8a960a2cb37
Jeffrey Varner <jdv27@cornell.edu>
2023-12-12 13:50:52 -0500

    change to types
    Types

--- src/Types.jl
+++ src/Types.jl
@@ -7,10 +7,7 @@ mutable struct MySimpleAgentModel <: AbstractAgentType
     wealth::Union{Nothing,Array{Float64,2}}
     shares::Union{Nothing,Array{Float64,2}}
     memory::Union{Nothing, Dict{Int64, CircularBuffer{Int}}}
-    
-    policy::Union{Nothing,Array{Dict{Vector{Int64}, Int64},1}}
     actions::Union{Nothing, Array{Dict{Int64, Float64}, 1}}
-    
     Q::Union{Nothing, Array{Float64,2}}
     states::Union{Nothing, Dict{Int64, Vector{Int64}}} # this is a map from index to the state vector
     coordinates::Union{Nothing, Dict{Vector{Int64}, Int64}} # this is a map from the state vector to the index
