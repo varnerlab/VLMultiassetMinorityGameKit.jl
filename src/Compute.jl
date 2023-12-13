0f76748474c0c5fbe32dbf93efc18dba54e01215
Jeffrey Varner <jdv27@cornell.edu>
2023-12-13 16:52:36 -0500

    Update Compute.jl
    show

--- src/Compute.jl
+++ src/Compute.jl
@@ -9,6 +9,8 @@ function _update_agent_memory(model::MySimpleAgentModel, data::Array{Int64,1})
         # get the memory -
         memory = model.memory[i];
 
+        @show memory
+
         # add the data -
         push!(memory, class);
     end
