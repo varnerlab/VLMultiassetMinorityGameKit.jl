cea660934554a8b693217b9d99e1a08fd2fd021a
Jeffrey Varner <jdv27@cornell.edu>
2023-12-13 06:56:36 -0500

    wealth update
    update

--- src/Compute.jl
+++ src/Compute.jl
@@ -49,6 +49,7 @@ function trade(model::MySimpleAgentModel, price::Array{Float64,1}, step::Int64; 
 
             # buffer is not full, so skip (no trade, shares stay the same) -
             model.shares[step+1,i] = model.shares[step,i];
+            _update_agent_wealth(model, price, step);
 
             # we don't have enough data to make a decision, so skip -
             continue;
