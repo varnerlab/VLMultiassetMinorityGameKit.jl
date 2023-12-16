377ea2d40d0e74e036b24aff684f53db989a5818
Jeffrey Varner <jdv27@cornell.edu>
2023-12-16 06:47:38 -0500

    Update Compute.jl
    step or step + 1?

--- src/Compute.jl
+++ src/Compute.jl
@@ -149,7 +149,7 @@ function trade(model::MySimpleAgentModel, price::Array{Float64,1}, step::Int64; 
 
                 # update the Q table -
                 budget = model.budget;
-                r = (1/budget)*(model.wealth[step+1,i] + new_balance)
+                r = (1/budget)*(model.wealth[step,i] + new_balance)
 
                 # update the Q table -
                 Q[s,aᵢ] = Q[s,aᵢ] + α*(r + γ*maximum(Q[s′,:]) - Q[s,aᵢ]);
