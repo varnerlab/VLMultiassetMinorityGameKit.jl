931686ef7b2b3b1bd543351deb4c7720a6d7446e
Jeffrey Varner <jdv27@cornell.edu>
2023-12-16 06:46:29 -0500

    Update Compute.jl
    remove penalty

--- src/Compute.jl
+++ src/Compute.jl
@@ -149,8 +149,7 @@ function trade(model::MySimpleAgentModel, price::Array{Float64,1}, step::Int64; 
 
                 # update the Q table -
                 budget = model.budget;
-                penalty = max(0.0, -1*(model.shares[step+1,i] - 0.01))^2;
-                r = (1/budget)*(model.wealth[step+1,i] + new_balance) - 10000.0*penalty
+                r = (1/budget)*(model.wealth[step+1,i] + new_balance)
 
                 # update the Q table -
                 Q[s,aᵢ] = Q[s,aᵢ] + α*(r + γ*maximum(Q[s′,:]) - Q[s,aᵢ]);
